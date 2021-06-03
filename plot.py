9835b9ab9696b7e7842ad813060fa65793be9fc1
Daniel Holmberg <daniel.holmberg97@gmail.com>
2021-06-04 00:09:15 +0300

    Add distribution, summary and flavour plots
    https://github.com/andrey-popov/ml-jec/blob/master/bin/plot_performance.py

--- plot.py
+++ plot.py
@@ -2,9 +2,11 @@ import os
 import argparse
 import warnings
 import pickle
+import itertools
 import awkward as ak
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from coffea.nanoevents import NanoEventsFactory, PFNanoAODSchema
 
@@ -51,7 +53,7 @@ def read_data(paths, predictions):
     return df
 
 
-def plot_loss(outdir, history):
+def plot_loss(history, outdir):
     plt.plot(history['loss'][1:], label='Training loss')
     plt.plot(history['val_loss'][1:], label='Validation loss')
     plt.xlabel('Epoch')
@@ -60,7 +62,7 @@ def plot_loss(outdir, history):
     plt.savefig(f'{outdir}/loss.png')
 
 
-def plot_mean_response(outdir, df, flavour):
+def plot_mean_response(df, flavour, outdir):
     binning = np.geomspace(30, 3000, 20)
     bin_centers = np.sqrt(binning[:-1] * binning[1:])
 
@@ -95,7 +97,7 @@ def plot_mean_response(outdir, df, flavour):
     fig.savefig(f'{outdir}/{flavour}_mean_response.png')
 
 
-def plot_mean_residual(outdir, df, flavour_1, flavour_2):
+def plot_mean_residual(df, flavour_1, flavour_2, outdir):
     binning = np.geomspace(30, 3000, 20)
     bin_centers = np.sqrt(binning[:-1] * binning[1:])
 
@@ -142,6 +144,229 @@ def plot_mean_residual(outdir, df, flavour_1, flavour_2):
     fig.savefig(f'{outdir}/{flavour_1[0]}_{flavour_2[0]}_mean_residual.png')
 
 
+def plot_distrs(dataframe, fig_dir):
+    """Plot distributions of response in a few representative bins."""
+
+    binning = np.linspace(0.5, 1.5, num=101)
+    pt_bins = [(30., 40.), (100., 110.), (1000., 1100.)]
+
+    ref_histograms, nn_histograms = {}, {}
+    for ipt, pt_bin in enumerate(pt_bins):
+        df_bin = dataframe[
+            (dataframe.GenJet_pt >= pt_bin[0]) & (dataframe.GenJet_pt < pt_bin[1])
+        ]
+        for label, selection in [
+            ('uds', (df_bin.flavour <= 3) & (df_bin.flavour != 0)),
+            ('b', df_bin.flavour == 5),
+            ('g', df_bin.flavour == 21)
+        ]:
+            h, _ = np.histogram(df_bin.response[selection], bins=binning)
+            ref_histograms[ipt, label] = h
+            h, _ = np.histogram(df_bin.dnn_response[selection], bins=binning)
+            nn_histograms[ipt, label] = h
+
+    for ipt, flavour in itertools.product(
+        range(len(pt_bins)), ['uds', 'b', 'g']
+    ):
+        fig = plt.figure()
+        axes = fig.add_subplot()
+        axes.hist(
+            binning[:-1], weights=ref_histograms[ipt, flavour],
+            bins=binning, histtype='step', label='Standard')
+        axes.hist(
+            binning[:-1], weights=nn_histograms[ipt, flavour],
+            bins=binning, histtype='step', label='DNN')
+        axes.axvline(1., ls='dashed', lw=0.8, c='gray')
+        axes.margins(x=0)
+        axes.set_xlabel(
+            r'$p_\mathrm{T}^\mathrm{corr}\//\/p_\mathrm{T}^\mathrm{ptcl}$')
+        axes.set_ylabel('Jets')
+        axes.legend()
+        axes.text(
+            1., 1.002,
+            r'${}$, ${:g} < p_\mathrm{{T}}^\mathrm{{ptcl}} < {:g}$ GeV, '
+            r'$|\eta^\mathrm{{ptcl}}| < 2.5$'.format(
+                flavour, pt_bins[ipt][0], pt_bins[ipt][1]
+            ),
+            ha='right', va='bottom', transform=axes.transAxes
+        )
+        fig.savefig(os.path.join(
+            fig_dir, f'{flavour}_pt{ipt + 1}.png'
+        ))
+        plt.close(fig)
+
+
+def bootstrap(x, num=30):
+    """Compute errors on median and IQR with bootstrapping."""
+
+    if len(x) == 0:
+        return np.nan, np.nan
+
+    medians, iqrs = [], []
+    for _ in range(num):
+        x_resampled = np.random.choice(x, len(x))
+        medians.append(np.median(x_resampled))
+        quantiles = np.percentile(x_resampled, [25, 75])
+        iqrs.append(quantiles[1] - quantiles[0])
+    return np.std(medians), np.std(iqrs)
+
+
+def compute_iqr(groups):
+    """Compute IQR from series GroupBy.
+
+    There is a bug in pandas when computing quantiles from a
+    GroupBy [1].
+    [1] https://github.com/pandas-dev/pandas/issues/33200
+    """
+
+    iqr = []
+    for _, series in groups:
+        values = series.to_numpy()
+        if len(values) <= 1:
+            iqr.append(np.nan)
+        else:
+            q = np.quantile(values, [0.25, 0.75])
+            iqr.append(q[1] - q[0])
+    print(iqr)
+    return np.asarray(iqr)
+
+
+def plot_summary(dataframe, fig_dir):
+    """Plot median response and its IQR as a function of pt."""
+
+    pt_binning = np.geomspace(20., 3e3, num=20)
+    pt_centres = np.sqrt(pt_binning[:-1] * pt_binning[1:])
+    for flavour_label, flavours in [
+            ('uds', {1, 2, 3}), ('b', {5}), ('g', {21}),
+            ('all', {0, 1, 2, 3, 4, 5, 21})
+        ]:
+
+        df_bin = dataframe[dataframe.flavour.isin(flavours)]
+        bins = df_bin.groupby(pd.cut(df_bin.GenJet_pt, pt_binning))
+
+        ref_median = bins.response.median().to_numpy()
+        ref_iqr = compute_iqr(bins.response)
+        ref_median_error = np.empty_like(ref_median)
+        ref_iqr_error = np.empty_like(ref_median)
+        for i, (_, df) in enumerate(bins):
+            ref_median_error[i], ref_iqr_error[i] = bootstrap(
+                df.response.to_numpy())
+
+        nn_median = bins.dnn_response.median().to_numpy()
+        nn_iqr = compute_iqr(bins.dnn_response)
+        nn_median_error = np.empty_like(ref_median)
+        nn_iqr_error = np.empty_like(ref_median)
+        for i, (_, df) in enumerate(bins):
+            nn_median_error[i], nn_iqr_error[i] = bootstrap(
+                df.dnn_response.to_numpy())
+
+        fig = plt.figure()
+        axes = fig.add_subplot()
+        axes.errorbar(
+            pt_centres, ref_median, yerr=ref_median_error,
+            marker='o', lw=0, elinewidth=0.8, label='Standard')
+        axes.errorbar(
+            pt_centres, nn_median, yerr=nn_median_error,
+            marker='o', lw=0, elinewidth=0.8, label='DNN')
+        axes.axhline(1., ls='dashed', lw=0.8, c='gray')
+        axes.set_xlim(pt_binning[0], pt_binning[-1])
+        axes.set_xscale('log')
+        axes.set_xlabel(r'$p_\mathrm{T}^\mathrm{ptcl}$')
+        axes.set_ylabel('Median response')
+        axes.legend()
+        axes.text(
+            1., 1.002,
+            '{}$|\\eta^\\mathrm{{ptcl}}| < 2.5$'.format(
+                f'${flavour_label}$, ' if flavour_label != 'all' else ''
+            ),
+            ha='right', va='bottom', transform=axes.transAxes
+        )
+        fig.savefig(os.path.join(
+            fig_dir, f'{flavour_label}_median.png'))
+        plt.close(fig)
+
+        fig = plt.figure()
+        gs = mpl.gridspec.GridSpec(2, 1, hspace=0.02, height_ratios=[4, 1])
+        axes_upper = fig.add_subplot(gs[0, 0])
+        axes_lower = fig.add_subplot(gs[1, 0])
+
+        axes_upper.errorbar(
+            pt_centres, ref_iqr / ref_median, yerr=ref_iqr_error / ref_median,
+            marker='o', lw=0, elinewidth=0.8, label='Standard')
+        axes_upper.errorbar(
+            pt_centres, nn_iqr / nn_median, yerr=nn_iqr_error / nn_median,
+            marker='o', lw=0, elinewidth=0.8, label='DNN')
+        axes_lower.plot(
+            pt_centres, (nn_iqr / nn_median) / (ref_iqr / ref_median),
+            marker='o', lw=0, color='black')
+
+        axes_upper.set_ylim(0., None)
+        axes_lower.set_ylim(0.85, 1.02)
+        for axes in [axes_upper, axes_lower]:
+            axes.set_xscale('log')
+            axes.set_xlim(pt_binning[0], pt_binning[-1])
+        axes_upper.xaxis.set_major_formatter(mpl.ticker.NullFormatter())
+        axes_upper.xaxis.set_minor_formatter(mpl.ticker.NullFormatter())
+        axes_upper.legend()
+        axes_upper.text(
+            1., 1.002,
+            '{}$|\\eta^\\mathrm{{ptcl}}| < 2.5$'.format(
+                f'${flavour_label}$, ' if flavour_label != 'all' else ''
+            ),
+            ha='right', va='bottom', transform=axes_upper.transAxes
+        )
+        axes_upper.set_ylabel('IQR / median for response')
+        axes_lower.set_ylabel('Ratio')
+        axes_lower.set_xlabel(r'$p_\mathrm{T}^\mathrm{ptcl}$')
+        fig.align_ylabels()
+
+        fig.savefig(os.path.join(
+            fig_dir, f'{flavour_label}_iqr.png'))
+        plt.close(fig)
+
+
+def compare_flavours(dataframe, fig_dir):
+    """Plot median response as a function of jet flavour."""
+
+    pt_cut = 30
+    df_pteta = dataframe[(dataframe.GenJet_pt > pt_cut)]
+
+    ref_median, ref_median_error = [], []
+    nn_median, nn_median_error = [], []
+    flavours = [('g', {21}), ('uds', {1, 2, 3}), ('c', {4}), ('b', {5})]
+    for _, pdg_ids in flavours:
+        df = df_pteta[df_pteta.flavour.isin(pdg_ids)]
+        ref_median.append(df.response.median())
+        ref_median_error.append(bootstrap(df.response)[0])
+        nn_median.append(df.dnn_response.median())
+        nn_median_error.append(bootstrap(df.dnn_response)[0])
+
+    fig = plt.figure()
+    axes = fig.add_subplot()
+    axes.errorbar(
+        np.arange(len(flavours)) - 0.02, ref_median, yerr=ref_median_error,
+        marker='o', ms=2, lw=0, elinewidth=0.8, label='Standard'
+    )
+    axes.errorbar(
+        np.arange(len(flavours)) + 0.02, nn_median, yerr=nn_median_error,
+        marker='o', ms=2, lw=0, elinewidth=0.8, label='DNN'
+    )
+    axes.set_xlim(-0.5, len(flavours) - 0.5)
+    axes.axhline(1, ls='dashed', lw=0.8, c='gray')
+    axes.set_xticks(np.arange(len(flavours)))
+    axes.set_xticklabels([f[0] for f in flavours])
+    axes.legend()
+    axes.set_ylabel('Median response')
+    axes.text(
+        1., 1.002,
+        r'$p_\mathrm{{T}}^\mathrm{{ptcl}} > {:g}$ GeV, '
+        r'$|\eta^\mathrm{{ptcl}}| < 2.5$'.format(pt_cut),
+        ha='right', va='bottom', transform=axes.transAxes
+    )
+    fig.savefig(os.path.join(fig_dir, 'flavours.png'))
+    plt.close(fig)
+
+
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description=__doc__)
     arg_parser.add_argument('-i', '--indir', required=True, help='Directory with dnn training results')
@@ -156,16 +381,26 @@ if __name__ == '__main__':
     with open(f'{args.indir}/history.pkl', 'rb') as f:
         history = pickle.load(f)
 
-    plot_loss(args.outdir, history)
+    plot_loss(history, args.outdir)
 
     with open(f'{args.indir}/predictions.pkl', 'rb') as f:
         predictions, test_files = pickle.load(f)
     
     df = read_data(test_files, predictions)
 
+    for subdir in ['distributions', 'summary', 'flavours', 'response', 'residual']:
+        try:
+            os.makedirs(os.path.join(args.outdir, subdir))
+        except FileExistsError:
+            pass
+
+    plot_distrs(df, os.path.join(args.outdir, 'distributions'))
+    plot_summary(df, os.path.join(args.outdir, 'summary'))
+    compare_flavours(df, os.path.join(args.outdir, 'flavours'))
+
     for flavour, ids in [
         ('uds', {1, 2, 3}), ('b', {5}), ('g', {21}), ('all', {0, 1, 2, 3, 4, 5, 21})
     ]:
-        plot_mean_response(args.outdir, df[df.flavour.isin(ids)], flavour)
+        plot_mean_response(df[df.flavour.isin(ids)], flavour, os.path.join(args.outdir, 'response'))
 
-    plot_mean_residual(args.outdir, df, ('g', {21}), ('uds', {1, 2, 3}))
+    plot_mean_residual(df, ('g', {21}), ('uds', {1, 2, 3}), os.path.join(args.outdir, 'residual'))
